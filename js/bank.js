b0c4a0fe48dac44887339a9a555d63a56a606e47
mabrur007 <mirmd.mabrur@gmail.com>
2022-02-11 23:44:36 +0600

    new function added

--- js/bank.js
+++ js/bank.js
@@ -3,10 +3,18 @@ document.getElementById('submit-btn').addEventListener('click', function (){
     // getting user's email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
-    console.log('User Email: ',userEmail);
+    // console.log('User Email: ',userEmail);
 
     // getting user's password
     const passwordField = document.getElementById('user-password');
     const userPassword  = passwordField.value;
-    console.log('User Password: ', userPassword);
+    // console.log('User Password: ', userPassword);
+
+    // redirect from index.html to banking.html page
+    if(userEmail == 'john@gmail.com' && userPassword == '1234'){
+        window.location.href = 'banking.html';
+    }
+    else{
+        console.log('Wrong email or password!');
+    }
 });
